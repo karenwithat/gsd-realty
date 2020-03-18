a0dfb447e482b893eb2cae0bc80790d9cc33392f
Tearyne Almendariz <tearyne.almendariz@lullabot.com>
2020-03-18 15:21:14 +0000

    Corrected incomplete git merge. removed debugging code from settings.php.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -23,13 +23,3 @@ $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
 }
-
-error_reporting(E_ALL);
-ini_set('display_errors', TRUE);
-ini_set('display_startup_errors', TRUE);
-$config['system.logging']['error_level'] = 'verbose';
-$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
-$config['system.performance']['css']['preprocess'] = FALSE;
-$config['system.performance']['js']['preprocess'] = FALSE;
-$settings['cache']['bins']['render'] = 'cache.backend.null';
-$settings['cache']['bins']['page'] = 'cache.backend.null';
